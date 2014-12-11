721e3c1f2f7dc97154f8c8c16a29832cd0ea00e2
Jeroen Hofstee <jeroen@myspectrum.nl>
2014-12-11 20:37:09 +0100

    fix weird backslah and be more verbose

--- test/test-dir-move.sh
+++ test/test-dir-move.sh
@@ -2,13 +2,13 @@
 mkdir d1
 touch d1/f1
 mv d1 d2
-fileuid=`\ls -n1 d2/f1 | awk '{ print $3 }'`
+fileuid=`ls -nl d2/f1 | awk '{ print $3 }'`
 if [ "$fileuid" == "$UID" ]
 then
-    #echo "Passed."
+    echo "Passed."
     rm -rf d2
     exit 0
 fi
 rm -rf d2
-#echo "Failed"
+echo "Failed"
 exit 1
